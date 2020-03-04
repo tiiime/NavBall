f2520006bd0065e8aeadda1006f512b2a0bb3f7c
kang <kjxstar@gmail.com>
2020-03-04 23:00:08 +0800

    simple main ui

--- app/src/main/java/me/invoker/navibutton/MainActivity.kt
+++ app/src/main/java/me/invoker/navibutton/MainActivity.kt
@@ -1,12 +1,35 @@
 package me.invoker.navibutton
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import me.invoker.navibutton.service.FloatingWindowService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        start.setOnClickListener {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this)) {
+                startService(Intent(this, FloatingWindowService::class.java))
+            } else {
+                Toast.makeText(this, getString(R.string.ask_for_permission), Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
+
+        stop.setOnClickListener {
+            stopService(Intent(this, FloatingWindowService::class.java))
+        }
+
+        back.setOnClickListener {
+            finish()
+        }
     }
 }
